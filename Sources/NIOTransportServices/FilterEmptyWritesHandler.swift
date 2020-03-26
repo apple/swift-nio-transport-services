a40188d9c7b73f3a456100ac0e58330a73bc54fb
Marcus Liotta <marcus@liotta.se>
2020-03-26 14:41:29 +0100

    Change copyright year Modify write guard to deal with non-empty writes with nil promise Improve completion order in flush Add connection state handling

--- Sources/NIOTransportServices/FilterEmptyWritesHandler.swift
+++ Sources/NIOTransportServices/FilterEmptyWritesHandler.swift
@@ -2,7 +2,7 @@
 //
 // This source file is part of the SwiftNIO open source project
 //
-// Copyright (c) 2017-2018 Apple Inc. and the SwiftNIO project authors
+// Copyright (c) 2020 Apple Inc. and the SwiftNIO project authors
 // Licensed under Apache License v2.0
 //
 // See LICENSE.txt for license information
@@ -20,10 +20,20 @@ import NIO
 /// A `ChannelHandler` that checks for outbound writes of zero length, which are then dropped. This is
 /// due to a bug in `Network Framework`, where zero byte TCP writes lead to stalled connections.
 /// Write promises are confirmed in the correct order.
-public final class FilterEmptyWritesHandler: ChannelOutboundHandler {
+public final class FilterEmptyWritesHandler: ChannelDuplexHandler {
+    public typealias InboundIn = ByteBuffer
+    public typealias InboundOut = ByteBuffer
     public typealias OutboundIn = ByteBuffer
     public typealias OutboundOut = ByteBuffer
 
+    private enum ChannelState {
+        case open
+        case closedFromRemote
+        case closedFromLocal
+        case error
+    }
+    
+    private var state: ChannelState = .closedFromLocal
     private var prefixEmptyWritePromise: Optional<EventLoopPromise<Void>>
     private var lastWritePromise: Optional<EventLoopPromise<Void>>
     
@@ -34,43 +44,102 @@ public final class FilterEmptyWritesHandler: ChannelOutboundHandler {
     
     public func write(context: ChannelHandlerContext, data: NIOAny, promise: EventLoopPromise<Void>?) {
         let buffer = self.unwrapOutboundIn(data)
-        if buffer.readableBytes > 0, let promise = promise {
-            self.lastWritePromise = promise
-        }
-        guard buffer.readableBytes == 0 else {
-            context.write(data, promise: promise)
-            return
-        }
-        
-        switch (self.prefixEmptyWritePromise, self.lastWritePromise, promise) {
-        case (_, _, .none): break
-        case (.none, .none, .some(let promise)): // first empty write with promise
-            self.prefixEmptyWritePromise = promise
-        case (_, .some(let lastWritePromise), .some(let promise)):
-            lastWritePromise.futureResult.cascade(to: promise)
-        case (.some(let prefixEmptyWritePromise), .none, .some(let promise)):
-            prefixEmptyWritePromise.futureResult.cascade(to: promise)
+        if buffer.readableBytes > 0 {
+            let lastWritePromise: EventLoopPromise<Void>
+            if let promise = promise {
+                lastWritePromise = promise
+            } else {
+                lastWritePromise = context.eventLoop.makePromise(of: Void.self)
+            }
+            self.lastWritePromise = lastWritePromise
+            context.write(data, promise: lastWritePromise)
+        } else {
+            /*
+             Empty writes needs to be handled individually depending on:
+             A) Empty write occurring before any non-empty write needs a
+             separate promise to cascade from (prefix)
+             B) Non-empty writes carry a promise, that subsequent empty
+             writes can cascade from
+             */
+            switch (self.prefixEmptyWritePromise, self.lastWritePromise, promise) {
+            case (_, _, .none): break
+            case (.none, .none, .some(let promise)):
+                self.prefixEmptyWritePromise = promise
+            case (_, .some(let lastWritePromise), .some(let promise)):
+                lastWritePromise.futureResult.cascade(to: promise)
+            case (.some(let prefixEmptyWritePromise), .none, .some(let promise)):
+                prefixEmptyWritePromise.futureResult.cascade(to: promise)
+            }
         }
     }
     
     public func flush(context: ChannelHandlerContext) {
-        self.prefixEmptyWritePromise?.succeed(())
-        self.prefixEmptyWritePromise = nil
         self.lastWritePromise = nil
-
-        context.flush()
+        if let prefixEmptyWritePromise = self.prefixEmptyWritePromise {
+            prefixEmptyWritePromise.futureResult.whenSuccess {
+                context.flush()
+            }
+            prefixEmptyWritePromise.succeed(())
+            self.prefixEmptyWritePromise = nil
+        } else {
+            context.flush()
+        }
     }
 }
 
-extension FilterEmptyWritesHandler: ChannelInboundHandler {
-    public typealias InboundIn = ByteBuffer
-    public typealias InboundOut = ByteBuffer
-
+// Connection state management
+extension FilterEmptyWritesHandler {
+    public func channelActive(context: ChannelHandlerContext) {
+        self.state = .open
+        context.fireChannelActive()
+    }
+    
     public func channelInactive(context: ChannelHandlerContext) {
-        self.prefixEmptyWritePromise?.fail(ChannelError.ioOnClosedChannel)
+        if case .open = self.state {
+            self.state = .closedFromRemote
+            self.prefixEmptyWritePromise?.fail(ChannelError.eof)
+        }
         self.prefixEmptyWritePromise = nil
         self.lastWritePromise = nil
-        
         context.fireChannelInactive()
+    }
+
+    public func close(context: ChannelHandlerContext, mode: CloseMode, promise: EventLoopPromise<Void>?) {
+        defer {
+            context.close(mode: mode, promise: promise)
+        }
+        
+        switch mode {
+        case .all:
+            if case .open = self.state {
+                self.state = .closedFromLocal
+                self.prefixEmptyWritePromise?.fail(ChannelError.ioOnClosedChannel)
+            }
+        case .output:
+            if case .open = self.state {
+                self.state = .closedFromLocal
+                self.prefixEmptyWritePromise?.fail(ChannelError.outputClosed)
+            }
+        case .input:
+            return
+        }
+        self.prefixEmptyWritePromise = nil
+        self.lastWritePromise = nil
+    }
+
+    public func errorCaught(context: ChannelHandlerContext, error: Error) {
+        if case .open = self.state {
+            self.state = .error
+            self.prefixEmptyWritePromise?.fail(error)
+        }
+        self.prefixEmptyWritePromise = nil
+        self.lastWritePromise = nil
+        context.fireErrorCaught(error)
+    }
+
+    public func handlerAdded(context: ChannelHandlerContext) {
+        if context.channel.isActive {
+            self.state = .open
+        }
     }
 }
